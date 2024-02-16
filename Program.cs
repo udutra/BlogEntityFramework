cbefdf84ccb216a9c41754b8be6c357e2256c2ff
Guilherme Galarça Dutra <udutra@gmail.com>
2024-02-16 18:33:31 -0300

    ThenInclude

--- Program.cs
+++ Program.cs
@@ -10,21 +10,18 @@ public static class Program
     public static void Main(string[] args)
     {
         using var context = new BlogDataContext();
-        var posts = GetPosts(context);
-        posts = GetPosts(context, 0 , 25);
-        posts = GetPosts(context, 25, 25);
-        posts = GetPosts(context, 50 , 25);
-        posts = GetPosts(context, 750 , 25);
-    }
 
-    private static List<Post> GetPosts(BlogDataContext context, int skip = 0, int take = 25)
-    {
-        var posts = context
-            .Posts
-            .AsNoTracking()
-            .Skip(skip)
-            .Take(take)
-            .ToList();
-        return posts;
+        var posts = context.Posts
+            .Include(x => x.Author)
+            .ThenInclude(x => x.Roles)
+            .Include(x => x.Tags);
+
+        foreach (var post in posts)
+        {
+            foreach (var tag in post.Tags)
+            {
+                
+            }
+        }
     }
 }
